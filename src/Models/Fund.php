3971ba7b6855b38d93641f7ca6111640e36eaeb5
Tadg Keating Webb <tadgkeating@gmail.com>
2023-10-18 15:07:25 +0700

    Models framework

---
+++ src/Models/Fund.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TadgKeatingWebb\EcoCreditModule\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Fund extends Model
+{
+
+}
---
+++ src/Models/Loan.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TadgKeatingWebb\EcoCreditModule\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Loan extends Model
+{
+
+}
